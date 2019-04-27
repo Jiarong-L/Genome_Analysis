a6b5e70c292b85933fe35db69040cfb10dd36403
Jiarong Liang <jili0178@rackham1.uppmax.uu.se>
2019-04-27 19:36:22 +0200

    pilon modify

--- code/sbatch_pilon_27m_0427.sh
+++ code/sbatch_pilon_27m_0427.sh
@@ -18,6 +18,6 @@ bwa index -a bwtsw /domus/h1/jili0178/GA2019/analyses_results/01_assemblyDNA/dur
 bwa mem /domus/h1/jili0178/GA2019/analyses_results/01_assemblyDNA/durian_pacbio_0411_27m/durian_pacbio_assembly_0411_27m.contigs.fasta /domus/h1/jili0178/GA2019/Genome_Analysis/data/illumina_DNA/SRR6058604_scaffold_06.1P.fastq.gz /domus/h1/jili0178/GA2019/Genome_Analysis/data/illumina_DNA/SRR6058604_scaffold_06.2P.fastq.gz > /domus/h1/jili0178/GA2019/analyses_results/02_polishDNA/bwa_for0411_0427.sam
 samtools view -Sb /domus/h1/jili0178/GA2019/analyses_results/02_polishDNA/bwa_for0411_0427.sam > /domus/h1/jili0178/GA2019/analyses_results/02_polishDNA/bwa_for0411_0427.bam
 samtools sort /domus/h1/jili0178/GA2019/analyses_results/02_polishDNA/bwa_for0411_0427.bam -o /domus/h1/jili0178/GA2019/analyses_results/02_polishDNA/bwa_for0411_0427.sorted.bam
-samtools index  -@ 10 -b /domus/h1/jili0178/GA2019/analyses_results/02_polishDNA/bwa_for0411_0427.sorted.bam /domus/h1/jili0178/GA2019/analyses_results/02_polishDNA/bwa_for0411_0427.sorted.bam.bai
+samtools index  -@ 10 /domus/h1/jili0178/GA2019/analyses_results/02_polishDNA/bwa_for0411_0427.sorted.bam
 
 pilon --genome /domus/h1/jili0178/GA2019/analyses_results/01_assemblyDNA/durian_pacbio_0411_27m/durian_pacbio_assembly_0411_27m.contigs.fasta --bam /domus/h1/jili0178/GA2019/analyses_results/02_polishDNA/bwa_for0411_0427.sorted.bam --output pilon_out_0427 --outdir /domus/h1/jili0178/GA2019/analyses_results/02_polishDNA/pilon_out_for0411_0427 --diploid --threads 4
