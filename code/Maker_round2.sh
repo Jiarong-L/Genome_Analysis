af15b21684fca74b196d9c4c171b66682fce3f8d
Jiarong Liang <jili0178@r148.uppmax.uu.se>
2019-05-14 23:11:33 +0200

    maker 2nd run

--- code/Maker_round2.sh
+++ code/Maker_round2.sh
@@ -2,19 +2,20 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 23:00:00
-#SBATCH -J optimize_augustus
+#SBATCH -t 15:00:00
+#SBATCH -J Maker_2nd
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user jiarong.liang.0178@student.uu.se
-
 # Load modules
 module load bioinfo-tools
 module load maker/3.01.1-beta
 module load blat/35
+# Your commands
 
-cd /domus/h1/jili0178/GA2019/analyses_results/04_annotation/round1_maker.output/
+cd /domus/h1/jili0178/GA2019/analyses_results/04_annotation/round1_maker.output
+
 export AUGUSTUS_CONFIG_PATH=/domus/h1/jili0178/GA2019/analyses_results/04_annotation/round1_maker.output/config
-optimize_augustus.pl --species=my_species augustus.gbk.train
-etraining --species=my_species augustus.gbk.train
-augustus --species=my_species augustus.gbk.test | tee second_training.out
 
+cd /domus/h1/jili0178/GA2019/analyses_results/04_annotation
+
+maker
