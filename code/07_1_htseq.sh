8c5c67ff5278faf37a790210049c1eb32bf85818
Jiarong000 <47032383+Jiarong000@users.noreply.github.com>
2019-05-28 11:24:45 +0200

    Update 07_1_htseq.sh

--- code/07_1_htseq.sh
+++ code/07_1_htseq.sh
@@ -15,18 +15,19 @@ module load htseq/0.9.1
 cd /domus/h1/jili0178/GA2019/analyses_results/07_differential_analyses/round1
 
 
-htseq-count -f bam -t CDS -i Name -r pos /domus/h1/jili0178/GA2019/analyses_results/06_assemblyRNA/sorted_tophat/SRR6040097_sorted.bam \
+htseq-count -f bam -t CDS -i Name -r pos --stranded=no /domus/h1/jili0178/GA2019/analyses_results/06_assemblyRNA/sorted_tophat/SRR6040097_sorted.bam \
 /domus/h1/jili0178/GA2019/analyses_results/04_annotation/round1_maker.output/genome.gff3 > rna_SRR6040097_counts.txt
 
-htseq-count -f bam -t CDS -i Name -r pos /domus/h1/jili0178/GA2019/analyses_results/06_assemblyRNA/sorted_tophat/SRR6040096_sorted.bam \
+htseq-count -f bam -t CDS -i Name -r pos --stranded=no /domus/h1/jili0178/GA2019/analyses_results/06_assemblyRNA/sorted_tophat/SRR6040096_sorted.bam \
 /domus/h1/jili0178/GA2019/analyses_results/04_annotation/round1_maker.output/genome.gff3 > rna_SRR6040096_counts.txt
 
-htseq-count -f bam -t CDS -i Name -r pos /domus/h1/jili0178/GA2019/analyses_results/06_assemblyRNA/sorted_tophat/SRR6040093_sorted.bam \
+htseq-count -f bam -t CDS -i Name -r pos --stranded=no /domus/h1/jili0178/GA2019/analyses_results/06_assemblyRNA/sorted_tophat/SRR6040093_sorted.bam \
 /domus/h1/jili0178/GA2019/analyses_results/04_annotation/round1_maker.output/genome.gff3 > rna_SRR6040093_counts.txt
 
-htseq-count -f bam -t CDS -i Name -r pos /domus/h1/jili0178/GA2019/analyses_results/06_assemblyRNA/sorted_tophat/SRR6040092_sorted.bam \
+htseq-count -f bam -t CDS -i Name -r pos --stranded=no /domus/h1/jili0178/GA2019/analyses_results/06_assemblyRNA/sorted_tophat/SRR6040092_sorted.bam \
 /domus/h1/jili0178/GA2019/analyses_results/04_annotation/round1_maker.output/genome.gff3 > rna_SRR6040092_counts.txt
 
 
 
 #The currect usage: add --stranded=no, since my RNA illumina method is not standard
+#Then -r name    ,since memory exceeded
